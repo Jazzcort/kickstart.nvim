b2406f0deb84c42a9089ea912d7dd0f68d9ea2cc
Jazzcort <jason101011113@gmail.com>
2026-05-26 11:28:37 -0400

    Replace pyright with ty

--- init.lua
+++ init.lua
@@ -602,9 +602,11 @@ require('lazy').setup({
       local servers = {
         -- clangd = {},
         gopls = {},
-        pyright = {},
+        -- pyright = {},
         rust_analyzer = {},
         svelte = {},
+        html = {},
+        ty = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
